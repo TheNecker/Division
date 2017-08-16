5f3fb7496c1c28f727852016b120194af31fcbac
TheNecker <chrisp132012@gmail.com>
2017-08-15 21:20:43 -0400

    Add files via upload

---
+++ Divide.rb
@@ -0,0 +1,11 @@
+def Division(num_1,num_2)
+
+	puts "#{num_1}/#{num_2} = #{num_1 / num_2}"
+end 
+print "What is your first number?"
+num_1 = gets.chomp.to_i
+puts "Ok, and your second number?"
+num_2 = gets.chomp.to_i
+
+
+Division(num_1,num_2) 
